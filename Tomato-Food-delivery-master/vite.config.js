f1d074f12d4f3e5f38680686b923fef6a7679f81
Sarayu-chityala <sarayuchityala15@gmail.com>
2025-10-02 09:02:32 +0530

    Deploy setup

--- Tomato-Food-delivery-master/vite.config.js
+++ Tomato-Food-delivery-master/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/MERN-food-delivery-webiste/"   // 👈 this should match your repo name exactly (case-sensitive)
 })
